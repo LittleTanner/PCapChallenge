1e2d16b9149658eaec49fd7f429356a113619d8c
Kevin Tanner <kevindavidtanner@gmail.com>
2020-08-06 14:06:40 -0700

    Sets up previousArticleCollectionViewCell

---
+++ PCapCodingChallenge/PCapCodingChallenge/KDTPreviousArticleCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  KDTPreviousArticleCollectionViewCell.m
+//  PCapCodingChallenge
+//
+//  Created by Kevin Tanner on 8/6/20.
+//  Copyright © 2020 Kevin Tanner. All rights reserved.
+//
+
+#import "KDTPreviousArticleCollectionViewCell.h"
+
+@implementation KDTPreviousArticleCollectionViewCell
+
+@end
